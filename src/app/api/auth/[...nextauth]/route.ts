e13954e40ff70770df0ba64ed45a6cac43993b76
AymerickCotche <aymerick.cotche@hotmail.fr>
2023-08-06 11:36:56 +0400

    next-auth options edited

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,8 @@
 import NextAuth from "next-auth"
-import type { AuthOptions } from "next-auth"
+import type { NextAuthOptions  } from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
 
-const authOptions: AuthOptions = {
+export const authOptions: NextAuthOptions  = {
   providers: [
     CredentialsProvider({
       name: 'Credentials',
