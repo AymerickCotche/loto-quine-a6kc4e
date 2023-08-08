4494a7499a6dfe595247fbc81de24da9af156d67
AymerickCotche <aymerick.cotche@hotmail.fr>
2023-08-08 07:28:34 +0400

    adding dispatch dependence to useEffect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
       pusherClient.unsubscribe('test')
       pusherClient.unbind('number:new', numberHandler)
     }
-  }, [])
+  }, [dispatch])
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
